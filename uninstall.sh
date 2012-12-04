4827acd84f9ac743bd28f5179c99e14edd180c8d
Tuomas Jorma Juhani Räsänen <tuomasjjrasanen@tjjr.fi>
2012-12-04 23:19:49 +0200

    Do not use sudo inside uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -4,13 +4,13 @@ set -u
 
 ROOT=/srv/debuilder
 
-sudo service rebuildd-httpd stop
-sudo service rebuildd stop
+service rebuildd-httpd stop
+service rebuildd stop
 
-sudo rm -rf $ROOT
-sudo rm /var/lib/rebuildd/rebuildd.db
+rm -rf $ROOT
+rm /var/lib/rebuildd/rebuildd.db
 
-sudo rm -f /etc/apache2/sites-available/debuilder
-sudo rm -f /etc/apache2/sites-enabled/debuilder
+rm -f /etc/apache2/sites-available/debuilder
+rm -f /etc/apache2/sites-enabled/debuilder
 
-sudo rm -f /etc/apt/sources.list.d/debuilder.list
+rm -f /etc/apt/sources.list.d/debuilder.list
